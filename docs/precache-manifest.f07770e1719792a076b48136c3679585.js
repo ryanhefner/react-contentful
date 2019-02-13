05830d95da3e60c8d72fb97bfc23c40714278432
Ryan Hefner <hi@ryanhefner.com>
2019-02-13 15:45:14 -0500

    Add docs site

---
+++ docs/precache-manifest.f07770e1719792a076b48136c3679585.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "356c9d7a07d877c3c29a",
+    "url": "/static/css/main.afb8925f.chunk.css"
+  },
+  {
+    "revision": "356c9d7a07d877c3c29a",
+    "url": "/static/js/main.356c9d7a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c68935a7e4a360cc5496",
+    "url": "/static/js/2.c68935a7.chunk.js"
+  },
+  {
+    "revision": "1ab367b91b302c10ddab87f6c8159870",
+    "url": "/index.html"
+  }
+];
