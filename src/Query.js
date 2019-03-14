295efa1fd2e60b6e3341343ac11e6901cfffcd18
Ryan Hefner <hi@ryanhefner.com>
2019-03-14 15:45:07 -0400

    Refactor Query component to support SSR requests

--- src/Query.js
+++ src/Query.js
@@ -24,109 +24,170 @@ class Query extends Component {
     }
   }
 
+  async validateRequestRequirements() {
+    return new Promise((resolve, reject) => {
+      const {
+        id,
+        contentType,
+        contentful,
+        include,
+        locale,
+        parser,
+        query,
+      } = this.props;
+
+      // Check for contentful context
+      warning(contentful, 'No contentful context passed to <Query />');
+
+      if (!contentful) {
+        return reject('No contentful context passed to <Query />');
+      }
+
+      const {
+        client,
+        locale: contextLocale,
+      } = contentful;
+
+      // Check to make sure a client is available
+      warning(client, 'ContentfulClient not available via context on <Query />');
+
+      if (!client) {
+        return reject('ContentfulClient not available via context on <Query />');
+      }
+
+      const hasQuery = !!(id || contentType || query === {});
+
+      // Check to make sure queryable props have been set
+      warning(hasQuery, 'Query props not set on <Query />');
+
+      if (!hasQuery) {
+        return reject('Query props not set on <Query />');
+      }
+
+      return resolve(true);
+    });
+  }
+
+  async fetchData() {
+    if (this.props.skip) {
+      return Promise.resolve(null);
+    }
+
+    return new Promise((resolve, reject) => {
+      const {
+        contentful,
+        contentType,
+        id,
+        include,
+        locale,
+        query,
+      } = this.props;
+
+      this.validateRequestRequirements().then(() => {
+        const {
+          client,
+          locale: contextLocale,
+          renderPromises,
+        } = contentful;
+
+        const requestLocale = locale || contextLocale;
+
+        const request = id
+          ? client.getEntry(id, {
+              locale: requestLocale,
+              include,
+              ...query
+            })
+          : client.getEntries({
+              'content_type': contentType,
+              locale: requestLocale,
+              include,
+              ...query
+            });
+
+        if (renderPromises) {
+          renderPromises.registerSSRObservable(this, request);
+        }
+
+        return request;
+      })
+      .then(resolve)
+      .catch(reject);
+    });
+  }
+
   requestContentfulData() {
     const {
-      id,
-      contentType,
-      contentful,
-      include,
-      locale,
       parser,
-      query,
       onRequest,
       onLoad,
       onError,
     } = this.props;
 
-    // Check for contentful context
-    warning(contentful, 'No contentful context passed to <Query />');
+    this.validateRequestRequirements().then(() => {
+      this.setState({
+        loading: true,
+      }, async () => {
+        onRequest(this.state);
 
-    if (!contentful) {
-      return;
-    }
-
-    const {
-      client,
-      locale: contextLocale,
-    } = contentful;
-
-    // Check to make sure a client is available
-    warning(client, 'ContentfulClient not available via context on <Query />');
-
-    if (!client) {
-      return;
-    }
-
-    const hasQuery = !!(id || contentType || query === {});
-
-    // Check to make sure queryable props have been set
-    warning(hasQuery, 'Query props not set on <Query />');
-
-    if (!hasQuery) {
-      return;
-    }
-
-    this.setState({
-      loading: true,
-    }, async () => {
-      onRequest(this.state);
-
-      const requestLocale = locale || contextLocale;
-      const request = await id
-        ? client.getEntry(id, {
-            locale: requestLocale,
-            include,
-            ...query
-          })
-        : client.getEntries({
-            'content_type': contentType,
-            locale: requestLocale,
-            include,
-            ...query
+        this.fetchData().then(response => {
+          this.setState({
+            data: parser(response),
+            loading: false,
+          }, () => {
+            onLoad(this.state);
           });
-
-      request.then(response => {
-        this.setState({
-          data: parser(response),
-          loading: false,
-        }, () => {
-          onLoad(this.state);
         });
-      }).catch(error => {
-        this.setState({
-          error,
-          loading: false,
-        }, () => {
-          onError(this.state);
-        });
+      });
+    })
+    .catch(error => {
+      this.setState({
+        error,
+        loading: false,
+      }, () => {
+        onError(this.state);
       });
     });
   }
 
+  getQueryResult() {
+    return this.state;
+  }
+
   render() {
-    if (!this.props.children) {
-      return null;
+    const {
+      children,
+      contentful,
+    } = this.props;
+
+    const finish = () => children(this.getQueryResult());
+
+    if (contentful && contentful.renderPromises) {
+      return contentful.renderPromises.addQueryPromise(this, finish);
     }
 
-    return (this.props.children(this.state));
+    return finish();
   }
 }
 
 Query.propTypes = {
   children: PropTypes.func,
-  entryId: PropTypes.string,
   contentType: PropTypes.string,
+  id: PropTypes.string,
   include: PropTypes.number,
   query: PropTypes.object,
   parser: PropTypes.func,
+  skip: PropTypes.bool,
   onRequest: PropTypes.func,
   onLoad: PropTypes.func,
   onError: PropTypes.func,
 };
 
 Query.defaultProps = {
+  children: ({data, error, loading}) => null,
   include: 10,
   query: {},
+  skip: false,
   parser: (data) => data,
   onRequest: () => {},
   onLoad: () => {},
