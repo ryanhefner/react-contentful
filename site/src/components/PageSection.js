ff5d3da9708834448dc0b216c0d23aed0a5686b1
Ryan Hefner <hi@ryanhefner.com>
2020-01-24 14:25:41 -0500

    Update PageSection in sample site to work with package updates

--- site/src/components/PageSection.js
+++ site/src/components/PageSection.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-const PageSection = ({ className = '', title = 'Title', description = {} }) => (
+const PageSection = ({ className = '', title = 'Title', description }) => (
   <section className={`App__page-section ${className}`}>
     <h2 className="App__page-section__title">{title}</h2>
-    <div className="App__page-section__description">
-      {documentToReactComponents(description)}
-    </div>
+    {description && (
+      <div className="App__page-section__description">
+        {documentToReactComponents(description)}
+      </div>
+    )}
   </section>
 );
 
