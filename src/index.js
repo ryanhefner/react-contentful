ee746a986d19cdea45773fe6cd57ba8e2dbd7402
Ryan Hefner <hi@ryanhefner.com>
2020-01-24 13:28:32 -0500

    Remove additional references to getDataFromTree and RenderPromises

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ export { default as ContentfulClient } from './ContentfulClient';
 export { default as ContentfulContext } from './ContentfulContext';
 export { default as ContentfulProvider } from './ContentfulProvider';
 export { default as Query } from './Query';
-export { default as RenderPromises } from './RenderPromises';
-export { default as getDataFromTree } from './getDataFromTree';
 export { default as withContentful } from './withContentful';
